4be824f06309ce259093efa0b4c8dc3925c8edc3
Edwin Pham <edwin.pham@yahoo.com>
2015-01-21 12:17:12 -0800

    Remapped sleep to first::zzz

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -17,7 +17,19 @@ class First extends Application
     function index()
     {
         $this->data['pagebody'] = 'justone';
+
         $source = $this->quotes->first();
+
+        $this->data = array_merge($this->data, $source);
+
+        $this->render();
+    }
+
+    function zzz()
+    {
+        $this->data['pagebody'] = 'justone';
+
+        $source = $this->quotes->get(1);
 
         $this->data = array_merge($this->data, $source);
 
