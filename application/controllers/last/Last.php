8a89d27b1c799ec44b8f4e3b0c7d9c0f6557be90
Edwin Pham <edwin.pham@yahoo.com>
2015-01-21 12:14:18 -0800

    Change the Last controller to Welcome controller inside the /last folder

--- application/controllers/last/Last.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Edwin
- * Date: 15-01-21
- * Time: 10:56 AM
- */
---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Edwin
+ * Date: 15-01-21
+ * Time: 10:56 AM
+ */
+class Welcome extends Application
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $this->data['pagebody'] = 'justone';
+
+        $source = $this->quotes->last();
+
+        $this->data = array_merge($this->data, $source);
+
+        $this->render();
+    }
+}
